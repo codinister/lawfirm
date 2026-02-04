1cc01059a4d21a2ec3afb8ce088115bce7753d0c
codinister <codinister@gmail.com>
2026-02-04 19:10:59 +0100

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
               </div>
               <div>
                 <p>Send Us A Message</p>
-                <h6 className="bread-words sm:max-w-[270px] max-w-[150px]">{email}</h6>
+                <h6 className="break-words sm:max-w-[270px] max-w-[150px]">{email}</h6>
               </div>
             </li>
           </ul>
