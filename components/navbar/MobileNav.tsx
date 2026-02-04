91500e25e3d2437190c708abfddeab2b9c0ac29d
codinister <codinister@gmail.com>
2026-02-04 13:49:31 +0100

    Added mobile navigation bar

--- components/navbar/MobileNav.tsx
+++ components/navbar/MobileNav.tsx
@@ -2,18 +2,67 @@
 
 import { Title } from '@radix-ui/react-dialog';
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
-import { CiMenuFries } from 'react-icons/ci';
+import { CiLocationOn, CiMenuFries } from 'react-icons/ci';
+import imagesData from '@/state/images/imagesData';
+import navData from './navData';
+import Image from 'next/image';
+import Link from 'next/link';
+import footerData from '@/state/footer/footerData';
+import { PiPhoneOutgoingLight } from 'react-icons/pi';
 
 const MobileNav = () => {
+  const { logo } = imagesData;
+  const { city, street, address, phone, note, email } = footerData;
+
   return (
-    <nav className="md:hidden block">
+    <nav className="md:hidden block bg-white w-full p-4 fixed top-0 left-0 z-19">
       <Sheet>
         <SheetTrigger>
-           <Title></Title>
-              <CiMenuFries className="cursor-pointer text-2xl" />
+          <Title></Title>
+          <CiMenuFries className="cursor-pointer text-3xl" />
         </SheetTrigger>
 
-        <SheetContent>dd</SheetContent>
+        <SheetContent className="p-6 w-[85%]">
+          <Image
+            src={logo}
+            alt="logo"
+            className="mb-20"
+            width="100"
+            height="100"
+          />
+          <ul>
+            {navData.map((v, k) => (
+              <li key={k} className="mb-7">
+                <Link href={v.path}>{v.name}</Link>
+              </li>
+            ))}
+          </ul>
+
+          <div className="mt-20">
+            <h5 className="mb-6 text-primary">Contact</h5>
+            <ul>
+              <li className="flex gap-6 mb-6">
+                <div>
+                  <CiLocationOn className="text-5xl text-primary" />
+                </div>
+                <div>
+                  <h6 className="text-primary">{city}</h6>
+                  <div>{street}</div>
+                  <div>{address}</div>
+                </div>
+              </li>
+              <li className="flex gap-6">
+                <div>
+                  <PiPhoneOutgoingLight className="text-5xl text-primary" />
+                </div>
+                <div>
+                  <div>{phone}</div>
+                  <div>{note}</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </SheetContent>
       </Sheet>
     </nav>
   );
